24ff861263700cf6e20559e738f5e7b1811cd4a2
Pavel <mankov.174@gmail.com>
2024-10-20 00:35:13 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 28_569;
+        int mile = 20;
+
+        int bonus = price / mile;
+
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
